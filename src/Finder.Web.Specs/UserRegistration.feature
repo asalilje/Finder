3d66608809e7703735f2e43b03eb2aa9620a81f5
Åsa <asa.lilje@gmail.com>
2014-01-29 00:49:30 +0100

    mer features

---
+++ src/Finder.Web.Specs/UserRegistration.feature
@@ -0,0 +1,16 @@
+﻿Feature: User Registration
+	In order to get access to the bacon info
+	As a potential new user
+	I want to create an account
+
+
+Scenario Outline: Password strength indicator
+Given I'm on the registration page
+When I enter a password of <password>
+Then the password strength indicator should read <strength>
+
+Examples: 
+| password  | strength |
+| pass      | Dålig    |
+| Password  | Hyfsad   |
+| Password1! | Suverän  |
---
+++ src/Finder.Web.Specs/UserRegistrationSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Finder.Web.Specs.Wrappers;
+using TechTalk.SpecFlow;
+
+namespace Finder.Web.Specs
+{
+    [Binding]
+    public class UserRegistrationSteps
+    {
+	    private readonly RegisterPageWrapper _wrapper;
+
+	    public UserRegistrationSteps(RegisterPageWrapper wrapper)
+				{
+					_wrapper = wrapper;
+				}
+
+	    [Given]
+				public void Given_I_m_on_the_registration_page()
+				{
+						_wrapper.GoToRegisterPage();
+				}
+
+				[When]
+				public void When_I_enter_a_password_of_PASSWORD(string password)
+				{
+						_wrapper.SetPassword(password);
+				}
+
+				[Then]
+				public void Then_the_password_strength_indicator_should_read_STRENGTH(string strength)
+				{
+						_wrapper.AssertPasswordStrength(strength);
+				}
+    }
+}
